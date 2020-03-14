965e8d55e4c0b54fecae1666f68d01f1c520cab8
Luigi Cruz <luigifcruz@gmail.com>
2020-03-13 23:04:37 -0300

    Fix dataset filepath.

--- DeepSwitch/main.py
+++ DeepSwitch/main.py
@@ -5,19 +5,19 @@ from model import DeepSwitch
 
 
 # Declare Global Settings
-root_dir = "/media/luigifcruz/HDD1/SETI/dogscats"
+root_dir = "/media/luigifcruz/HDD1/SETI/fft_signal"
 input_size = (3, 512//4, 384//4)
 num_classes = 7
 learning_rate = 0.00001
-min_lr = 0.000000001
-patience = 10
+min_lr = 0.00000001
+patience = 5
 batch_size = 8
 epochs = 750
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Declare Interation Settings
 interations = [
-    {"model": DeepSwitch, "save_dir": 'runs/e6-s2-020713', "net_size": 1},
+    {"model": DeepSwitch, "save_dir": 'runs/v10', "net_size": 1},
 ]
 
 if __name__ == "__main__":
